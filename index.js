1717ab18951052c33a06ba845988094ad127bc61
Howard Chen <chenh0886@gmail.com>
2017-08-14 00:16:20 -0700

    Setup for heroku and requests

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.set('port', (process.env.PORT || 8000));
+
+app.get('/', function(request, response) {
+  response.send('Initialized')
+});
+
+app.get('/visibleEffect', function(req, res) {
+  res.json([
+    {"id": 0, "counter": 0}
+  ]);
+});
+
+app.get('/tracks', function(req, res) {
+  res.json([
+    {"id": 0, "divisions": 4},
+    {"id": 1, "divisions": 16}
+  ]);
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Running on localhost:" + app.get('port')); 
+});
