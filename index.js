5d7be7e2eaf34dd06e53e93778230648615346cc
Howard Chen <chenh0886@gmail.com>
2017-08-18 15:25:08 -0700

    Test new build to travis

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.get('/tracks', function(req, res) {
     {"id": 0, "divisions": 4},
     {"id": 1, "divisions": 16},
     {"id": 2, "divisions": 3},
-    {"id": 3, "divisions": 8}
+    {"id": 3, "divisions": 8},
+    {"id": 4, "divisions": 2}
   ]);
 });
 
