545571aa206803a7ae9b5d2717ddbd29f70dfd37
Howard Chen <chenh0886@gmail.com>
2017-09-14 23:45:21 -0700

    Write deleteTrack mutation

--- schema/mutations/index.js
+++ schema/mutations/index.js
@@ -69,7 +69,7 @@ const mutation = new GraphQLObjectType({
         uploadAudioFile: {
             type: UploadedFileType,
             args: {
-                trackid: {type: GraphQLInt }
+                trackid: { type: GraphQLInt }
             },
             resolve: (rootValue, args) => {
                 let encoded = rootValue.files[0].buffer.toString('base64');
@@ -86,6 +86,17 @@ const mutation = new GraphQLObjectType({
             resolve: (rootValue, args) => {
                 return createTrackToDB().then(
                     res => pubsub.publish('trackCreated', {trackCreated: res})
+                )
+            }
+        },
+        deleteTrack: {
+            type: GraphQLBoolean,
+            args: {
+                trackid: { type: GraphQLInt }
+            },
+            resolve: (args) => {
+                return deleteTrackFromDB(args.trackid).then(
+                    res => pubsub.publish('trackDeleted', {trackDeleted: res})
                 )
             }
         }
